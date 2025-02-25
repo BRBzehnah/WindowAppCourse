229ba2ac15c2c4c44cf5617880bc31d57699f749
Andrey Obvintsev <obvintsev2001@mail.ru>
2025-02-25 22:45:11 +0500

    Start of course project

---
+++ TaskManagerCourse/TaskManagerCourse.Api/Models/Desk.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerCourse.Api.Models
+{
+    public class Desk:CommonObject
+    {
+        public bool IsPrivat { get; set; }
+
+        public string? Columns { get; set; }
+
+        public User? Admin { get; set; }
+    }
+}
---
+++ TaskManagerCourse/TaskManagerCourse.Api/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerCourse.Api.Models
+{
+    public class Project:CommonObject
+    {
+        public List<User>? AllUsers { get; set; }
+
+        public List<Desk>? AllDesks { get; set; }
+    }
+}
